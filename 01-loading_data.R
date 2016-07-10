9bf1511fcdac49357ce2de61611790e9cd0364b7
adambondarzewski <adambondarzewski@gmail.com>
2016-07-10 16:10:12 +0200

    strings as factors arg added

--- 01-loading_data.R
+++ 01-loading_data.R
@@ -1,8 +1,12 @@
 library(data.table)
 
-DT_train <- as.data.table(read.csv("http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/train.csv"))
-DT_test <- as.data.table(read.csv("http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/test.csv"))
+DT_train <- as.data.table(read.csv("http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/train.csv",
+                                   stringsAsFactors = FALSE))
+DT_test <- as.data.table(read.csv("http://s3.amazonaws.com/assets.datacamp.com/course/Kaggle/test.csv",
+                                  stringsAsFactors = FALSE))
 
-DT_gender_model <- as.data.table(read.csv("https://www.kaggle.com/c/titanic/download/gendermodel.csv"))
-DT_gender_class_model <- as.data.table(read.csv("https://www.kaggle.com/c/titanic/download/genderclassmodel.csv"))
+DT_gender_model <- as.data.table(read.csv("https://www.kaggle.com/c/titanic/download/gendermodel.csv",
+                                          stringsAsFactors = FALSE))
+DT_gender_class_model <- as.data.table(read.csv("https://www.kaggle.com/c/titanic/download/genderclassmodel.csv",
+                                                stringsAsFactors = FALSE))
 
