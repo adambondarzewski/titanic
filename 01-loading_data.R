ae398509df31713bb11ac1af71b8292a22a170b5
patyk11 <patyk11@gmail.com>
2016-07-10 16:14:30 +0200

    basic stats

--- 01-loading_data.R
+++ 01-loading_data.R
@@ -6,3 +6,19 @@ DT_test <- as.data.table(read.csv("http://s3.amazonaws.com/assets.datacamp.com/c
 DT_gender_model <- as.data.table(read.csv("https://www.kaggle.com/c/titanic/download/gendermodel.csv"))
 DT_gender_class_model <- as.data.table(read.csv("https://www.kaggle.com/c/titanic/download/genderclassmodel.csv"))
 
+table(DT_train$Sex, DT_train$Survived)
+
+str(DT_train)
+str(DT_test)
+
+# Survival rates in absolute numbers
+table(DT_train$Survived)
+
+# Survival rates in proportions
+
+prop.table(table(DT_train$Survived))
+# Two-way comparison: Sex and Survived
+
+table(train$Sex, train$Survived)
+# Two-way comparison: row-wise proportions
+prop.table(table(train$Sex, train$Survived),1)
